2bfc368c90c00f0873760f55fb8d0d12917f6ecd
ziggs97 <zivkovic97nemanja@gmail.com>
2023-05-31 12:55:27 +0200

    loginAuth

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) { }
+
+  async login(email: string, password: string): Promise<void> {
+    try {
+      await this.afAuth.signInWithEmailAndPassword(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async register(email: string, password: string): Promise<void> {
+    try {
+      await this.afAuth.createUserWithEmailAndPassword(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async logout(): Promise<void> {
+    try {
+      await this.afAuth.signOut();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async resetPassword(email: string): Promise<void> {
+    try {
+      await this.afAuth.sendPasswordResetEmail(email);
+    } catch (error) {
+      throw error;
+    }
+  }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -8,13 +10,33 @@ import { Router } from '@angular/router';
 })
 export class Tab1Page {
 
-  constructor(private router:Router) {}
+  email= '';
+  password='';
+
+  constructor(private router:Router, private authService: AuthService,private toastController: ToastController) {}
 
   ngOnit(){}
 
-  login(){
-    this.router.navigate(['/tabs/tab2']);
+  async login(): Promise<void> {
+    try {
+      await this.authService.login(this.email, this.password);
+      // Login successful
+      this.router.navigate(['/tabs/tab2']); // Navigate to Tab2
+    } catch (error) {
+      // Handle login error
+      const toast = await this.toastController.create({
+        message: 'Invalid login data.',
+        duration: 3000,
+        position: 'bottom',
+        color: 'danger',
+      });
+      toast.present();
+    }
   }
+
+  // login(){
+  //   this.router.navigate(['/tabs/tab2']);
+  // }
 
   register(){
     this.router.navigate(['register']);
