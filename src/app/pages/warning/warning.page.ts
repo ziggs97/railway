3ba20f15f83cc02c5b1f6e59d2d7b38a1eede63d
ziggs97 <zivkovic97nemanja@gmail.com>
2023-06-19 22:47:15 +0200

    addWarning

--- src/app/pages/warning/warning.page.ts
+++ src/app/pages/warning/warning.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Firestore, collection, addDoc } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-warning',
@@ -8,14 +10,49 @@ import { Router } from '@angular/router';
 })
 export class WarningPage implements OnInit {
 
-  constructor(private router:Router) { }
+  title="";
+  country="";
+  neighborhood="";
+  address="";
+  city="";
+  validRamp = false;
+
+  constructor(private router:Router, private firestore: Firestore) { }
   
 
   ngOnInit() {
   }
 
-  listWarnings(){
-    this.router.navigate(['tabs/tab3'])
+  async addWarning() {
+    try {
+      const warning = {
+        title: this.title,
+        country: this.country,
+        neighborhood: this.neighborhood,
+        address: this.address,
+        city: this.city,
+        validRamp: this.validRamp
+      };
+
+      const warningsCollection = collection(this.firestore, 'warnings');
+      await addDoc(warningsCollection, warning);
+      console.log('Warning added successfully!');
+      
+      // Reset the form fields after adding the warning
+      this.title = '';
+      this.country = '';
+      this.neighborhood = '';
+      this.address = '';
+      this.city = '';
+      this.validRamp = false;
+    } catch (error) {
+      console.error('Error adding warning:', error);
+      // Handle the error, such as displaying an error message
+    }
   }
+
+  // addWarning(){
+  //   this.router.navigate(['tabs/tab3'])
+  // }
 
 }
