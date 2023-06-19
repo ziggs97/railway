803b371b784e8d3a661c42977b9143de7683905a
ziggs97 <zivkovic97nemanja@gmail.com>
2023-06-19 22:59:38 +0200

    resetPasswordFixed

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -31,23 +31,36 @@ export class Tab1Page {
       });
   }
 
+  //funkcionise
   async resetPassword(): Promise<void> {
     try {
-      const userExists = await this.authService.checkUserExists(this.email);
-      if (userExists) {
-        await this.authService.resetPassword(this.email);
-        // Password reset email sent successfully
-        // Do something, such as displaying a success message
-        this.presentToast('Password reset email sent successfully');
-      } else {
-        throw new Error('User not found');
-      }
-    } catch (error) {
+      await this.authService.resetPassword(this.email);
+    }
+    catch (error) {
       console.error('Password reset email sending failed:', error);
       // Handle the error, such as displaying an error message
       this.presentToast('Failed to send password reset email');
     }
   }
+
+  //bolje, ali ne funkcionise
+  // async resetPassword(): Promise<void> {
+  //   try {
+  //     const userExists = await this.authService.checkUserExists(this.email);
+  //     if (userExists) {
+  //       await this.authService.resetPassword(this.email);
+  //       // Password reset email sent successfully
+  //       // Do something, such as displaying a success message
+  //       this.presentToast('Password reset email sent successfully');
+  //     } else {
+  //       throw new Error('User not found');
+  //     }
+  //   } catch (error) {
+  //     console.error('Password reset email sending failed:', error);
+  //     // Handle the error, such as displaying an error message
+  //     this.presentToast('Failed to send password reset email');
+  //   }
+  // }
 
   private async presentToast(message: string): Promise<void> {
     const toast = await this.toastController.create({
